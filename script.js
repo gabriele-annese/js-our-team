eecabe82ed14a9cf20a2dd11469204ac38373428
gabriele-annese <gannese06@gmail.com>
2021-11-04 14:05:08 +0100

    add array info

---
+++ script.js
@@ -0,0 +1,40 @@
+// - Creiamo il nostro array di oggetti che rappresentano ciascun membro del team. Ogni membro dovrà avere le informazioni necessarie per stampare la relativa card: Nome, Ruolo e Foto.
+// - Prendendo come riferimento il layout di esempio presente nell’html, stampiamo tutte le card del nostro team.
+// - Utilizziamo poi gli input presenti nella pagina per permettere all’utente di aggiungere nuovi membri del team.
+
+
+// creazione array di oggetti
+const teamMember = [
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/wayne-barnett-founder-ceo.jpg',
+    },
+    {
+        name: 'Walter Gordon',
+        job: 'Office manager',
+        img: 'img/walter-gordon-office-manager.jpg',
+    },
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/scott-estrada-developer.jpg',
+    },
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/angela-caroll-chief-editor.jpg',
+    },
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/angela-lopez-social-media-manager.jpg',
+    },
+    {
+        name: 'Wayne Barnett',
+        job: 'Founder & CEO',
+        img: 'img/angela-caroll-chief-editor.jpg',
+    },
+];
+
+// creare nodo dove inserire card
